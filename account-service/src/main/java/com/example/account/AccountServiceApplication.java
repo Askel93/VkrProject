4555f80ccf479de8ce57c3947f983c5974ee42e3
mitinikitos <mityushkin.nik.99@gmail.com>
2021-05-18 21:34:19 +0300

    create account service

---
+++ account-service/src/main/java/com/example/account/AccountServiceApplication.java
@@ -0,0 +1,25 @@
+package com.example.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients(basePackages = "com.example.account")
+@EnableCircuitBreaker
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableJpaRepositories(basePackages = "com.example.account.repository")
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
