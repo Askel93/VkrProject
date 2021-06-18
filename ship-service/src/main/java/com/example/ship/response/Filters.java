aa288d655da2d2310499ec39b6b4d33119c3eef4
mitinikitos <mityushkin.nik.99@gmail.com>
2021-06-18 18:56:56 +0300

    add filters

--- ship-service/src/main/java/com/example/ship/response/Filters.java
+++ ship-service/src/main/java/com/example/ship/response/Filters.java
@@ -2,7 +2,6 @@ package com.example.ship.response;
 
 import com.example.ship.config.View;
 import com.fasterxml.jackson.annotation.JsonView;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,44 +15,16 @@ public class Filters {
 	/*
 	* Ship filter
 	*/
-	@JsonView(View.UI.class)
-	private ShipFilterRes shipFilter;
-	@JsonView(View.UI.class)
-	private DimensionsFilterRes dimensionsFilter;
-	@JsonView(View.UI.class)
-	private CapacityFilterRes capacityFilter;
-
-	@Data
-	@Builder
-	public static class ShipFilterRes {
-		private MinMaxFilter<Integer> speed;
-		private MinMaxFilter<Integer> godP;
-		private List<String> type; //Type ship
-		private List<String> port; //Port ship
-	}
-	private MinMaxFilter<Integer> speed;
-	private MinMaxFilter<Integer> godP;
-	private List<String> type; //Type ship
-	private List<String> port; //Port ship
+	private Integer ns;
+	private Integer xs;
+	private Integer ng;
+	private Integer xg;
+	private List<String> ts; //Type ship
+	private List<String> ps; //Port ship
 
 	/*
 	* Capacity filter
 	*/
-	@Data
-	@Builder
-	public static class CapacityFilterRes {
-		private MinMaxFilter<Integer> dedv;
-		private MinMaxFilter<Integer> passK;
-		private MinMaxFilter<Integer> passP;
-		private MinMaxFilter<Integer> nt;
-		private MinMaxFilter<Integer> gt;
-	}
-	private MinMaxFilter<Integer> dedv;
-	private MinMaxFilter<Integer> passK;
-	private MinMaxFilter<Integer> passP;
-	private MinMaxFilter<Integer> nt;
-	private MinMaxFilter<Integer> gt;
-
 	private Integer nd; //Min dead weight
 	private Integer xd; //Max dead weight
 	private Integer npk; //Min number of cabins
@@ -68,20 +39,6 @@ public class Filters {
 	/*
 	* Dimensions filter
 	*/
-	@Data
-	@Builder
-	public static class DimensionsFilterRes {
-		private MinMaxFilter<Integer> disp;
-		private MinMaxFilter<Double> length;
-		private MinMaxFilter<Double> breadth;
-		private MinMaxFilter<Double> draught;
-		private MinMaxFilter<Double> depth;
-	}
-	private MinMaxFilter<Integer> disp;
-	private MinMaxFilter<Double> length;
-	private MinMaxFilter<Double> breadth;
-	private MinMaxFilter<Double> draught;
-	private MinMaxFilter<Double> depth;
 	private Integer ndp; //Min displacement
 	private Integer xdp; //Max displacement
 	private Double nl; //Min length
@@ -96,47 +53,53 @@ public class Filters {
 	/*
 	* Engines filter
 	*/
-	private MinMaxFilter<Integer> sumPwr;
 	private Integer npwr; //Min sum power
 	private Integer xpwr; //Max sum power
 
 	public void fromCapacityFilter(CapacityFilter filter) {
-		capacityFilter = CapacityFilterRes.builder()
-				.dedv(new MinMaxFilter<>(filter.getMinDedv(), filter.getMaxDedv()))
-				.passK(new MinMaxFilter<>(filter.getMinPassK(), filter.getMaxPassK()))
-				.passP(new MinMaxFilter<>(filter.getMinPassP(), filter.getMaxPassP()))
-				.nt(new MinMaxFilter<>(filter.getMinNt(), filter.getMaxNt()))
-				.gt(new MinMaxFilter<>(filter.getMinGt(), filter.getMaxGt()))
-				.build();
+		nd = filter.getMinDedv();
+		xd = filter.getMaxDedv();
+		npk = filter.getMinPassK();
+		xpk = filter.getMaxPassK();
+		npp = filter.getMinPassP();
+		xpp = filter.getMaxPassP();
+		nnt = filter.getMinNt();
+		xnt = filter.getMaxNt();
+		ngt = filter.getMinGt();
+		xgt = filter.getMaxGt();
 	}
 	public void fromShipFilter(ShipFilter filter) {
-		shipFilter = ShipFilterRes.builder()
-				.speed(new MinMaxFilter<>(filter.getMinSpeed(), filter.getMaxSpeed()))
-				.godP(new MinMaxFilter<>(filter.getMinGodP(), filter.getMaxGodP()))
-				.type(filter.getTypeFilter())
-				.port(filter.getPortFilter())
-				.build();
+		ts = filter.getTypeFilter();
+		ps = filter.getPortFilter();
+		ns = filter.getMinSpeed();
+		xs = filter.getMaxSpeed();
+		ng = filter.getMinGodP();
+		xg = filter.getMaxGodP();
 	}
 
 	public void fromDimensionsFilter(DimensionsFilter filter) {
-		dimensionsFilter = DimensionsFilterRes.builder()
-				.disp(new MinMaxFilter<>(filter.getMinDisp(), filter.getMaxDisp()))
-				.length(new MinMaxFilter<>(filter.getMinLength(), filter.getMaxLength()))
-				.breadth(new MinMaxFilter<>(filter.getMinBreadth(), filter.getMaxBreadth()))
-				.draught(new MinMaxFilter<>(filter.getMinDraught(), filter.getMaxDraught()))
-				.depth(new MinMaxFilter<>(filter.getMinDepth(), filter.getMaxDepth()))
-				.build();
+		ndp = filter.getMinDisp();
+		xdp = filter.getMaxDisp();
+		nl = filter.getMinLength();
+		xl = filter.getMaxLength();
+		nb = filter.getMinBreadth();
+		xb = filter.getMaxBreadth();
+		ndt = filter.getMinDraught();
+		xdt = filter.getMaxDraught();
+		ndh = filter.getMinDepth();
+		xdh = filter.getMaxDepth();
 	}
 
 	public void fromEnginesFilter(EnginesFilter filter) {
-		sumPwr = new MinMaxFilter<>(filter.getMinPwr(), filter.getMaxPwr());
+		npwr = filter.getMinPwr();
+		xpwr = filter.getMaxPwr();
 	}
 
 	public boolean isShipFilterEmpty() {
-		return type == null
-				&& speed == null
-				&& godP == null
-				&& port == null;
+		return ts == null
+				&& ns == null
+				&& ng == null
+				&& ps == null;
 	}
 
 	public boolean isCapacityFilterEmpty() {
