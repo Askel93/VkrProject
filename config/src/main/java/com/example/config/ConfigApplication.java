39891f257cb3769a767a4922c12c71406b506dae
mitinikitos <mityushkin.nik.99@gmail.com>
2021-06-18 18:44:39 +0300

    edit config files

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -3,6 +3,9 @@ package com.example.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,6 +13,23 @@ public class ConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@Configuration
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http
+					.authorizeRequests()
+					.antMatchers("/actuator/**").permitAll()
+					.anyRequest().authenticated()
+
+					.and()
+					.httpBasic()
+			;
+		}
 	}
 
 }
