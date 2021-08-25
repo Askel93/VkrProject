81cb81600c5c2b6ca5f1aac2c592b733215881ec
mitinikitos <mityushkin.nik.99@gmail.com>
2021-08-25 14:50:32 +0300

    refactor for test

--- ship-service/src/main/java/com/example/ship/ShipServiceApplication.java
+++ ship-service/src/main/java/com/example/ship/ShipServiceApplication.java
@@ -6,30 +6,27 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
-import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
-@SpringBootApplication(exclude = {
-		TaskExecutionAutoConfiguration.class,
-		TaskSchedulingAutoConfiguration.class,
-		WebSocketServletAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableOAuth2Client
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableJpaRepositories(
-		basePackages = "com.example.ship.repository.impl",
-		repositoryBaseClass = BaseRepositoryImpl.class
+	basePackages = "com.example.ship.repository.impl",
+	repositoryBaseClass = BaseRepositoryImpl.class
 )
 @EnableCaching
+@EnableCircuitBreaker
+@EnableHystrix
 public class ShipServiceApplication {
 
 	public static void main(String[] args) {
@@ -37,13 +34,18 @@ public class ShipServiceApplication {
 	}
 
 	@Bean
-	public MappingJackson2HttpMessageConverter objectMapper() {
-		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+	public ObjectMapper objectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
-		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-		jsonConverter.setObjectMapper(mapper);
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		mapper.enable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+		mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		return mapper;
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+		jsonConverter.setObjectMapper(objectMapper());
 		return jsonConverter;
 	}
 }
